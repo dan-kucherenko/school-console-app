6edf2e5503e2690a248582380efde872607be39b
Daniil Kucherenko <daniil.ukraine@gmail.com>
2023-08-04 22:33:17 +0300

    Classes that represents the records from the db

---
+++ src/main/java/ua/foxminded/kucherenko/task2/models/Student.java
@@ -0,0 +1,51 @@
+package ua.foxminded.kucherenko.task2.models;
+
+public class Student {
+    private final int id;
+    private int groupId;
+    private final String firstName;
+    private final String lastName;
+
+    public Student(int id, int groupId, String firstName, String lastName) {
+        this.id = id;
+        this.groupId = groupId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Student(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", groupId=" + groupId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
