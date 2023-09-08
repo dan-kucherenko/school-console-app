0670fba8696227c58db59c8c4bcbb18e09474994
Daniil Kucherenko <daniil.ukraine@gmail.com>
2023-09-09 01:23:01 +0300

    GroupDaoTest fix

--- src/main/java/ua/foxminded/kucherenko/task2/models/Group.java
+++ src/main/java/ua/foxminded/kucherenko/task2/models/Group.java
@@ -39,4 +39,19 @@ public class Group {
                 ", groupName='" + groupName + '\'' +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Group group = (Group) o;
+
+        return getGroupName() != null ? getGroupName().equals(group.getGroupName()) : group.getGroupName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getGroupName() != null ? getGroupName().hashCode() : 0;
+    }
 }
--- src/test/java/ua/foxminded/kucherenko/task2/dao/GroupDaoTest.java
+++ src/test/java/ua/foxminded/kucherenko/task2/dao/GroupDaoTest.java
@@ -9,44 +9,62 @@ import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import ua.foxminded.kucherenko.task2.models.Group;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Testcontainers
-@Sql({"/database/create_tables.sql", "/database/clear_tables.sql"})
 class GroupDaoTest {
     @Autowired
     private GroupDao groupDao;
 
     @Test
-    @Sql("/sample_data/groups_samples.sql")
-    void get() {
-        final int groupId = 22;
+    @Sql({"/database/drop_tables.sql", "/database/create_tables.sql", "/sample_data/groups_samples.sql"})
+    void getGroupById() {
+        final int groupId = 1;
+        final String groupName = "OC-26";
         final Optional<Group> group = groupDao.get(groupId);
         Assertions.assertFalse(group.isEmpty());
+        Assertions.assertEquals(group.get().getGroupName(), groupName);
     }
 
     @Test
-    @Sql("/sample_data/groups_samples.sql")
-    void getAll() {
+    @Sql({"/database/drop_tables.sql", "/database/create_tables.sql", "/sample_data/groups_samples.sql"})
+    void getAllGroups() {
         final int groupsNumber = 10;
+        final List<Group> expectedGroups = List.of(
+                new Group("OC-26"),
+                new Group("AH-94"),
+                new Group("NO-69"),
+                new Group("KL-46"),
+                new Group("PN-91"),
+                new Group("ZT-54"),
+                new Group("CA-79"),
+                new Group("CF-85"),
+                new Group("QY-95"),
+                new Group("HV-40")
+        );
         final List<Group> groups = groupDao.getAll();
         Assertions.assertEquals(groupsNumber, groups.size());
+        Assertions.assertEquals(expectedGroups, groups);
     }
 
     @Test
-    @Sql({"/database/clear_tables.sql", "/sample_data/groups_samples.sql"})
+    @Sql({"/database/drop_tables.sql", "/database/create_tables.sql", "/sample_data/groups_samples.sql"})
     void getAllGroupIds() {
         final int groupsNumber = 10;
-        final List<Integer> groups = groupDao.getAllGroupIds();
-        Assertions.assertEquals(groupsNumber, groups.size());
+        final Integer[] expectedGroupIds = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        final List<Integer> groupsIds = groupDao.getAllGroupIds();
+        Assertions.assertEquals(groupsNumber, groupsIds.size());
+        Assertions.assertEquals(Arrays.stream(expectedGroupIds).toList(), groupsIds);
     }
 
 
     @Test
-    void save() {
+    @Sql({"/database/drop_tables.sql", "/database/create_tables.sql"})
+    void saveGroup() {
         final Group group = new Group("TestGroup");
         final int addedGroupId = 1;
         Assertions.assertTrue(groupDao.get(addedGroupId).isEmpty());
@@ -55,10 +73,9 @@ class GroupDaoTest {
     }
 
     @Test
-    void update() {
-        final Group group = new Group("TestGroup");
+    @Sql("/sample_data/insert_group.sql")
+    void updateGroup() {
         final int addedGroupId = 1;
-        groupDao.save(group);
         Assertions.assertNotNull(groupDao.get(addedGroupId));
         final Group changedGroup = new Group("TestGroup1");
         groupDao.update(addedGroupId, changedGroup);
@@ -66,10 +83,9 @@ class GroupDaoTest {
     }
 
     @Test
-    void delete() {
-        final Group group = new Group("TestGroup");
+    @Sql("/sample_data/insert_group.sql")
+    void deleteGroup() {
         final int addedGroupId = 1;
-        groupDao.save(group);
         groupDao.delete(addedGroupId);
         Assertions.assertNotNull(groupDao.get(addedGroupId));
     }
