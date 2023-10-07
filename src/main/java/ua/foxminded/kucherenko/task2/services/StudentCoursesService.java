b2137496fce3759838891796a12ceeab3c5e7b83
Daniil Kucherenko <daniil.ukraine@gmail.com>
2023-10-07 18:06:55 +0300

    Service layer edititng

--- src/main/java/ua/foxminded/kucherenko/task2/services/StudentCoursesService.java
+++ src/main/java/ua/foxminded/kucherenko/task2/services/StudentCoursesService.java
@@ -4,43 +4,89 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ua.foxminded.kucherenko.task2.dao.StudentCourseDao;
-import ua.foxminded.kucherenko.task2.dao.StudentDao;
+import ua.foxminded.kucherenko.task2.models.Course;
+import ua.foxminded.kucherenko.task2.models.Student;
+import ua.foxminded.kucherenko.task2.repositories.CourseRepository;
+import ua.foxminded.kucherenko.task2.repositories.StudentRepository;
 import ua.foxminded.kucherenko.task2.services.service_utils.add_student_to_course.AddStudentToCourseData;
 import ua.foxminded.kucherenko.task2.services.service_utils.remove_from_course.RemoveFromCourseData;
 
+import java.util.Optional;
+
 @Service
 public class StudentCoursesService {
-    @Autowired
-    private StudentCourseDao studentCourseDao;
-    @Autowired
-    private StudentDao studentDao;
+    private StudentRepository studentRepository;
+    private CourseRepository courseRepository;
     private static final Logger LOGGER = LoggerFactory.getLogger(StudentCoursesService.class);
+
+    @Autowired
+    public StudentCoursesService(StudentRepository studentRepository, CourseRepository courseRepository) {
+        this.studentRepository = studentRepository;
+        this.courseRepository = courseRepository;
+    }
 
     public void addStudentToCourse(AddStudentToCourseData data) {
         if (data.getCourseId() <= 0) {
             throw new IllegalArgumentException("Invalid course id: it should be more than 0");
         }
-        Integer studentId = studentDao.getIdByName(data.getFirstName(), data.getLastName());
+        Integer studentId = studentRepository.getIdByName(data.getFirstName(), data.getLastName());
         if (studentId == null) {
             throw new IllegalArgumentException("Invalid student id: student id is less than 0 or student doesn't exist");
         }
-        if (studentCourseDao.exists(studentId, data.getCourseId())) {
+        if (exists(studentId, data.getCourseId())) {
             throw new IllegalArgumentException("This record already exists");
         }
-        studentCourseDao.save(studentId, data.getCourseId());
+        save(studentId, data.getCourseId());
         LOGGER.debug("Student with id {} was successfully added to course {}", studentId, data.getCourseId());
     }
 
     public void removeStudentFromCourse(RemoveFromCourseData data) {
-        if (data.getCourseId()<= 0 || data.getCourseId() > 10) {
+        if (data.getCourseId() <= 0 || data.getCourseId() > 10) {
             throw new IllegalArgumentException("Course Id should be between 1 and 10");
         }
-        Integer studentId = studentDao.getIdByName(data.getFirstName(), data.getLastName());
+        Integer studentId = studentRepository.getIdByName(data.getFirstName(), data.getLastName());
         if (studentId == null) {
             throw new IllegalArgumentException("Student doesn't exist or the studentId is incorrect");
         }
-        studentCourseDao.delete(studentId, data.getCourseId());
+        delete(studentId, data.getCourseId());
         LOGGER.debug("Student with id {} was successfully removed from course {}", studentId, data.getCourseId());
+    }
+
+    private boolean exists(int studentId, int courseId) {
+        Optional<Student> student = studentRepository.findById(studentId);
+        if (student.isEmpty()) {
+            throw new IllegalArgumentException("Student with given id wasn't found");
+        }
+        Optional<Course> course = courseRepository.findById(courseId);
+        if (course.isEmpty()) {
+            throw new IllegalArgumentException("Course with given id wasn't found");
+        }
+        return student.get().getCourses().contains(course.get());
+    }
+
+    private void save(int studentId, int courseId) {
+        Optional<Student> student = studentRepository.findById(studentId);
+        if (student.isEmpty()) {
+            throw new IllegalArgumentException("Student with given id wasn't found");
+        }
+        Optional<Course> course = courseRepository.findById(courseId);
+        if (course.isEmpty()) {
+            throw new IllegalArgumentException("Course with given id wasn't found");
+        }
+        student.get().getCourses().add(course.get());
+        studentRepository.save(student.get());
+    }
+
+    private void delete(int studentId, int courseId) {
+        Optional<Student> student = studentRepository.findById(studentId);
+        if (student.isEmpty()) {
+            throw new IllegalArgumentException("Student with given id wasn't found");
+        }
+        Optional<Course> course = courseRepository.findById(courseId);
+        if (course.isEmpty()) {
+            throw new IllegalArgumentException("Course with given id wasn't found");
+        }
+        student.get().getCourses().remove(course.get());
+        studentRepository.save(student.get());
     }
 }
--- src/main/java/ua/foxminded/kucherenko/task2/services/StudentService.java
+++ src/main/java/ua/foxminded/kucherenko/task2/services/StudentService.java
@@ -4,9 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ua.foxminded.kucherenko.task2.dao.GroupDao;
-import ua.foxminded.kucherenko.task2.dao.StudentDao;
 import ua.foxminded.kucherenko.task2.models.Student;
+import ua.foxminded.kucherenko.task2.repositories.GroupRepository;
+import ua.foxminded.kucherenko.task2.repositories.StudentRepository;
 import ua.foxminded.kucherenko.task2.services.service_utils.add_student.AddStudentData;
 import ua.foxminded.kucherenko.task2.services.service_utils.delete_student.DeleteStudentData;
 import ua.foxminded.kucherenko.task2.services.service_utils.find_stud_by_course.FindStudentByCourseData;
@@ -15,13 +15,17 @@ import java.util.List;
 
 @Service
 public class StudentService {
-    @Autowired
-    private StudentDao studentDao;
-    @Autowired
-    private GroupDao groupDao;
+    private StudentRepository studentRepository;
+    private GroupRepository groupRepository;
     private static final Logger LOGGER = LoggerFactory.getLogger(StudentService.class);
     private static final int MAX_GROUP_CAPACITY = 30;
     private static final int NO_GROUP_INDEX = 0;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository, GroupRepository groupRepository) {
+        this.studentRepository = studentRepository;
+        this.groupRepository = groupRepository;
+    }
 
     public void addStudent(AddStudentData data) {
         if (data.getGroupId() < 0) {
@@ -29,12 +33,12 @@ public class StudentService {
         }
         Integer groupId = data.getGroupId() == NO_GROUP_INDEX ? null : data.getGroupId();
         if (groupId != null) {
-            int groupQuantity = groupDao.getGroupQuantity(data.getGroupId());
+            int groupQuantity = groupRepository.getGroupQuantity(data.getGroupId());
             if (groupIsFull(groupQuantity)) {
                 throw new IllegalArgumentException("Group is full");
             }
         }
-        studentDao.save(new Student(groupId, data.getFirstName(), data.getLastName()));
+        studentRepository.save(new Student(groupId, data.getFirstName(), data.getLastName()));
         LOGGER.debug("Student {} {} from group {} was successfully added", data.getFirstName(), data.getLastName(), data.getGroupId());
     }
 
@@ -42,16 +46,16 @@ public class StudentService {
         if (data.getCourseName().isBlank()) {
             throw new IllegalArgumentException("Course name can't be blank");
         }
-        List<Student> resultList = studentDao.getByCourse(data.getCourseName());
+        List<Student> resultList = studentRepository.getByCourse(data.getCourseName());
         LOGGER.debug("Students by course {} was successfully found", data.getCourseName());
         return resultList;
     }
 
     public void deleteStudent(DeleteStudentData data) {
-        if (data.getStudentId() <= 0 || studentDao.get(data.getStudentId()).isEmpty()) {
+        if (data.getStudentId() <= 0 || studentRepository.findById(data.getStudentId()).isEmpty()) {
             throw new IllegalArgumentException("Student ID doesn't exist");
         }
-        studentDao.delete(data.getStudentId());
+        studentRepository.deleteById(data.getStudentId());
         LOGGER.debug("Student with id {} was successfully deleted", data.getStudentId());
     }
 
